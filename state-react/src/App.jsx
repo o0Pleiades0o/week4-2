3573e314a7f6ecbe5db996d4bb20550624e1e20b
o0Pleiades0o <redkarroy@gmail.com>
2025-02-01 10:07:38 +0700

    1

--- state-react/src/App.jsx
+++ state-react/src/App.jsx
@@ -3,23 +3,66 @@ import { useState } from 'react'
 function App() {
   const [msg, setmsg] = useState("");
   const [todo, setTodo] = useState([]);
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [editingText, setEditingText] = useState("");
+
+  const isEditing = editingIndex !== null;
+
+  function edit(id) {
+    setEditingIndex(id);
+    setEditingText(todo[id]);
+  }
+
+  function save() {
+    if (editingIndex !== null) {
+      setTodo(
+        todo.map((item, index) => {
+          if (index === editingIndex) {
+            return editingText;
+          }
+          return item;
+        })
+      );
+      setEditingIndex(null);
+    }
+  }
 
   return (
     <>
-      <input type="text" onChange={(e) => setmsg(e.target.value)} value={msg} placeholder="Enter your message" />
-      <button onClick={() => setTodo([...todo, msg])} className='btn btn-primary'>Save</button>
-      <ol>
-        {todo.map((item, index) => {
-          return (
-            <li className='list-group-item p-2' key={index}>
-              {index + 1}. {item}
-              <button className='btn btn-danger ms-2' onClick={() => setTodo(todo.filter((_, i) => i !== index))}>
-                Delete
+      <div className="container">
+        <h1 className='text-center my-3 text-primary fw-bold'>Todo List</h1>
+        <input className='form-control my-3' type="text" onChange={(e) => setmsg(e.target.value)} value={msg} placeholder="Enter your message" />
+        <button className='btn btn-primary' onClick={() => setTodo([...todo, msg])}>Save</button>
+        <div className='card my-3'>
+          {todo.map((item, index) => {
+            return (
+              <div className='card-body' key={index}>
+                <h5 className='card-title'>{index + 1}. {item}</h5>
+                <button className='btn btn-primary' onClick={() => edit(index)}>
+                  Edit
+                </button>
+                <button className='btn btn-danger ms-2' onClick={() => setTodo(todo.filter((_, i) => i !== index))}>
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+        </div>
+        {isEditing && (
+          <div className='card my-3'>
+            <div className='card-body'>
+              <h5 className='card-title'>Edit</h5>
+              <input className='form-control' type="text" value={editingText} onChange={(e) => setEditingText(e.target.value)} />
+              <button className='btn btn-primary mt-2' onClick={save}>
+                Save
               </button>
-            </li>
-          );
-        })}
-      </ol>
+              <button className='btn btn-danger mt-2 ms-2' onClick={() => setEditingIndex(null)}>
+                Cancel
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </>
   )
 }
